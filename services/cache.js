1ef871f91af5e26982bde8c280191d3cf2e47b7e
Ademola Sobaki <demolasobaki@gmail.com>
2020-07-28 23:14:44 +0100

    cnditionally use cache

--- services/cache.js
+++ services/cache.js
@@ -6,11 +6,21 @@ const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl)
 
 client.get = util.promisify(client.get)
+
+// override original mongoose exec function, with ours to use caching
 const exec = mongoose.Query.prototype.exec;
 
+// conditionally use caching
+mongoose.Query.prototype.cache = function () {
+    this.useCache = true
+    return this;
+}
 
 
 mongoose.Query.prototype.exec = async function () {
+    if (!this.useCache === false) {
+        return exec.apply(this, arguments)
+    }
 
     const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
