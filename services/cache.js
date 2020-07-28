273eecf306a10b5c1035a057f9f6e61f7c0fb271
Ademola Sobaki <demolasobaki@gmail.com>
2020-07-28 23:29:48 +0100

    use nested hash as redis cache data structure

--- services/cache.js
+++ services/cache.js
@@ -4,15 +4,16 @@ const redis = require('redis')
 
 const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl)
-
-client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
 
 // override original mongoose exec function, with ours to use caching
 const exec = mongoose.Query.prototype.exec;
 
 // conditionally use caching
-mongoose.Query.prototype.cache = function () {
+mongoose.Query.prototype.cache = function (options = {}) {
     this.useCache = true
+    this.hashKey = JSON.stringify(options.key || '')
+
     return this;
 }
 
@@ -26,7 +27,7 @@ mongoose.Query.prototype.exec = async function () {
         collection: this.mongooseCollection.name
     }));
 
-    const cacheValue = await client.get(key)
+    const cacheValue = await client.hget(this.hashKey, key)
 
     if (cacheValue) {
         const doc = JSON.parse(cacheValue)
@@ -37,7 +38,7 @@ mongoose.Query.prototype.exec = async function () {
     }
 
     const result = await exec.apply(this, arguments)
-    client.set(key, JSON.stringify(result), 'EX', 10)
+    client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10)
 
     return result
 }
