8b3e5e0966256096383cf2ba0be2e4ad790e8c15
Ademola Sobaki <demolasobaki@gmail.com>
2020-07-28 23:06:29 +0100

    write cache logic

--- services/cache.js
+++ services/cache.js
@@ -6,17 +6,28 @@ const redisUrl = 'redis://127.0.0.1:6379'
 const client = redis.createClient(redisUrl)
 
 client.get = util.promisify(client.get)
-
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-    console.log('running the query')
 
-    const key = Object.assign({}, this.getQuery(), {
+
+mongoose.Query.prototype.exec = async function () {
+
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), {
         collection: this.mongooseCollection.name
-    });
+    }));
 
-    console.log('key', key)
+    const cacheValue = await client.get(key)
 
-    return exec.apply(this, arguments)
+    if (cacheValue) {
+        const doc = JSON.parse(cacheValue)
+
+        return Array.isArray(doc)
+            ? doc.map(d => new this.model(d))
+            : new this.model(doc);
+    }
+
+    const result = await exec.apply(this, arguments)
+    client.set(key, JSON.stringify(result))
+
+    return result
 }
